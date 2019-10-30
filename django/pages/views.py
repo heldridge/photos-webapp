2931d5d05985b0955f631cb984742484743ce1dd
Harry <MerlinCibula@protonmail.com>
2019-10-30 17:12:59 -0400

    Add secondary tailwind page

--- django/pages/views.py
+++ django/pages/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def tailwind(request):
+    return render(request, 'pages/index-tailwind.html.j2')
+
 def index(request):
     return render(request, 'pages/index.html.j2')
