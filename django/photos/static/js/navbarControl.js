590a7775f6c421b30703fac25864161086068a91
Harry <MerlinCibula@protonmail.com>
2019-11-01 17:33:43 -0400

    Refactor js

--- django/photos/static/js/navbarControl.js
+++ django/photos/static/js/navbarControl.js
@@ -1,3 +1,6 @@
+let searchBorder = document.getElementById("navSearchBorder");
+let navSearchIcon = document.getElementById("navSearchIcon");
+
 // Class manipulation functions from: https://jaketrent.com/post/addremove-classes-raw-javascript/
 function hasClass(ele,cls) {
     return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
@@ -14,36 +17,18 @@ function removeClass(ele,cls) {
     }
 }
 
-function setNavSearchBorderGray() {
-    let searchBorder = document.getElementById("navSearchBorder");
-    removeClass(searchBorder, 'border-blue-300');
-    addClass(searchBorder, 'border-gray-300');
-}
-
-function setNavSearchBorderBlue() {
-    let searchBorder = document.getElementById("navSearchBorder");
+function navSearchFocus() {
     removeClass(searchBorder, 'border-gray-300');
     addClass(searchBorder, 'border-blue-300');
-}
 
-function setNavSearchIconOpacity50() {
-    let navSearchIcon = document.getElementById("navSearchIcon");
-    removeClass(navSearchIcon, 'opacity-100');
-    addClass(navSearchIcon, 'opacity-50');
-}
-
-function setNavSearchIconOpacity100() {
-    let navSearchIcon = document.getElementById("navSearchIcon");
     removeClass(navSearchIcon, 'opacity-50');
     addClass(navSearchIcon, 'opacity-100');
 }
 
-function navSearchFocus() {
-    setNavSearchBorderBlue();
-    setNavSearchIconOpacity100();
-}
-
 function navSearchFocusOut() {
-    setNavSearchBorderGray();
-    setNavSearchIconOpacity50();
+    removeClass(searchBorder, 'border-blue-300');
+    addClass(searchBorder, 'border-gray-300');
+
+    removeClass(navSearchIcon, 'opacity-100');
+    addClass(navSearchIcon, 'opacity-50');
 }
