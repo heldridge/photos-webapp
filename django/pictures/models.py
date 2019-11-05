799422d6e5bdbffbd1d1b34c462f8f0b781b73da
Harry <MerlinCibula@protonmail.com>
2019-11-05 11:43:46 -0500

    Update picture model

--- django/pictures/models.py
+++ django/pictures/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
@@ -5,8 +7,18 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Picture(models.Model):
     title = models.CharField(max_length=100)
-    tags = ArrayField(models.CharField(max_length=20))
+    description = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/user_uploads/')
+    tags = ArrayField(models.CharField(max_length=20))
+    # uploaded_by = ForeignKey
+    uploaded_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    # galleries = ForeignKey (many to many???)
 
     def __str__(self):
         return self.title
+
+
+"""
+{"id": 0, "title": "Blue Cone", "description": "", "filename": "aaa.jpg", "tags": ["food", "blue", "dessert", "ice-cream"], "uploaded_by": "admin", "uploaded_at": 1560894129000, "updated_at": 1560894129000, "galleries": []}
+"""
