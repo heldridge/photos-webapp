1672a10339675a8f5f35f30770b67be297c839f2
Harry <MerlinCibula@protonmail.com>
2019-11-05 11:28:34 -0500

    Change images upload dir to user_uplaods

--- django/pictures/models.py
+++ django/pictures/models.py
@@ -6,7 +6,7 @@ from django.contrib.postgres.fields import ArrayField
 class Picture(models.Model):
     title = models.CharField(max_length=100)
     tags = ArrayField(models.CharField(max_length=20))
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = models.ImageField(upload_to='photos/user_uploads/')
 
     def __str__(self):
         return self.title
