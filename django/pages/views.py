474bfbf03f150dcf21970953323ddcd8dcfc0c4c
Harry <eldridgemharry@gmail.com>
2019-11-05 15:12:44 -0500

    Finish dynamic index

--- django/pages/views.py
+++ django/pages/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 
 from pictures.models import Picture
 
+
 # Create your views here.
 def tailwind(request):
     pictures = Picture.objects.order_by('uploaded_at')[:16]
     context = {
-        'pictures': pictures
+        'pictures': pictures,
+        'grid_placeholders': [1, 2]
     }
+    return render(request, 'pages/index-tailwind.html.j2', context)
 
-    return render(request, 'pages/index-tailwind-dynamic.html.j2', context)
-    # return render(request, 'pages/index-tailwind.html.j2')
 
 def index(request):
     return render(request, 'pages/index.html.j2')
