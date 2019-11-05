8861d64d1e48b71f786af29d19c9f71db350bc7f
Harry <MerlinCibula@protonmail.com>
2019-11-05 10:35:07 -0500

    Add Picture model

--- django/pictures/models.py
+++ django/pictures/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Picture(models.Model):
+    title = models.CharField(max_length=100)
+    tags = ArrayField(models.CharField(max_length=20))
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.title
