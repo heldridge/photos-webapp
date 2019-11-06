b22b8ccb09021a192fc7e0402a9d5d956c777f5b
Harry <MerlinCibula@protonmail.com>
2019-11-06 15:12:55 -0500

    Convert to typescript

---
+++ js/imagesGridFooterControl.ts
@@ -0,0 +1,16 @@
+let cards: HTMLCollection = document.getElementsByClassName('h-picture-card');
+let cardsHolder: HTMLElement = document.getElementById('cardsHolder');
+let previousButton: HTMLElement = document.getElementById('previousButton');
+let nextButton: HTMLElement = document.getElementById('nextButton');
+positionFooterButtons();
+
+function positionFooterButtons(): void {
+  if (cards.length > 0) {
+    let margin: string = `${cards[0].getBoundingClientRect().left -
+      cardsHolder.getBoundingClientRect().left}px`;
+    previousButton.style.marginLeft = margin;
+    nextButton.style.marginRight = margin;
+  }
+}
+
+window.addEventListener('resize', positionFooterButtons);
