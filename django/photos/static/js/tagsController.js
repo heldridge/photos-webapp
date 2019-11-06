a0bb636860a23061ff0e0c14126fc78f8c3bb47e
Harry <eldridgemharry@gmail.com>
2019-11-06 13:39:42 -0500

    Add button to collapse tags after expansion

--- django/photos/static/js/tagsController.js
+++ django/photos/static/js/tagsController.js
@@ -1,7 +1,16 @@
-function expandTags(collapser) {
-    let hiddenTags = collapser.parentElement.getElementsByClassName('collapsed-tag');
+function expandTags(expander) {
+    let hiddenTags = expander.parentElement.getElementsByClassName('collapsed-tag');
     for (i = 0; i < hiddenTags.length; i ++) {
         removeClass(hiddenTags[i], 'hidden');
     }
-    addClass(collapser, 'hidden');
+    addClass(expander, 'hidden');
+}
+
+function collapseTags(collapser) {
+    let hiddenTags = collapser.parentElement.getElementsByClassName('collapsed-tag');
+    for (i = 0; i < hiddenTags.length; i ++) {
+        addClass(hiddenTags[i], 'hidden');
+    }
+    let expander = collapser.parentElement.getElementsByClassName('tag-expander')[0];
+    removeClass(expander, 'hidden');
 }
