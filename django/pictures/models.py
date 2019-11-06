10a8a5dc7ea275c0e1587e91a9087bf0ebdd2b86
Harry <MerlinCibula@protonmail.com>
2019-11-06 16:09:50 -0500

    Allow no tags

--- django/pictures/models.py
+++ django/pictures/models.py
@@ -9,7 +9,7 @@ class Picture(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/user_uploads/')
-    tags = ArrayField(models.CharField(max_length=20))
+    tags = ArrayField(models.CharField(max_length=20), blank=True)
     # uploaded_by = ForeignKey
     uploaded_at = models.DateTimeField()
     updated_at = models.DateTimeField()
@@ -17,8 +17,3 @@ class Picture(models.Model):
 
     def __str__(self):
         return self.title
-
-
-"""
-{"id": 0, "title": "Blue Cone", "description": "", "filename": "aaa.jpg", "tags": ["food", "blue", "dessert", "ice-cream"], "uploaded_by": "admin", "uploaded_at": 1560894129000, "updated_at": 1560894129000, "galleries": []}
-"""
