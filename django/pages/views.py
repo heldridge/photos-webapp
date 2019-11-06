c771aaa2d1b0896fcd6d666fb047ae25d328868a
Harry <eldridgemharry@gmail.com>
2019-11-06 11:41:48 -0500

    Hide tags after a certain tag length has been reached

--- django/pages/views.py
+++ django/pages/views.py
@@ -5,9 +5,29 @@ from pictures.models import Picture
 
 # Create your views here.
 def tailwind(request):
-    pictures = Picture.objects.order_by('uploaded_at')[:16]
+    pictures = Picture.objects.order_by('-uploaded_at')[:16]
+
+    max_width = 85
+    static_width_addition = 4
+    updated_pictures = []
+    for picture in pictures:
+        data = {'original': picture, 'above_tags': [], 'below_tags': []}
+        current_width = 0
+        above = True
+        for tag in picture.tags:
+            current_width += static_width_addition + len(tag)
+            if current_width > max_width:
+                above = False
+
+            if above:
+                data['above_tags'].append(tag)
+            else:
+                data['below_tags'].append(tag)
+
+        updated_pictures.append(data)
+
     context = {
-        'pictures': pictures,
+        'pictures': updated_pictures,
         'grid_placeholders': [1, 2]
     }
     return render(request, 'pages/index-tailwind.html.j2', context)
