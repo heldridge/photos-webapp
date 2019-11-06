d3ca25c3186d2857f754ff8ab1bcde9dc6a1702e
Harry <MerlinCibula@protonmail.com>
2019-11-06 13:34:49 -0500

    Rework showing hidden tags so new tags stay in line as collapser dissapears

--- django/photos/static/js/tagsController.js
+++ django/photos/static/js/tagsController.js
@@ -1,5 +1,7 @@
 function expandTags(collapser) {
-    let hiddenTags = collapser.parentElement.nextElementSibling;
-    removeClass(hiddenTags, 'hidden');
+    let hiddenTags = collapser.parentElement.getElementsByClassName('collapsed-tag');
+    for (i = 0; i < hiddenTags.length; i ++) {
+        removeClass(hiddenTags[i], 'hidden');
+    }
     addClass(collapser, 'hidden');
 }
