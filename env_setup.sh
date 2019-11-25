2cab9ff6849dd46dbf605fb2c527e7572e5c3c2e
Harry Eldridge <heldridge@protonmail.com>
2019-11-25 17:26:52 -0500

    Move settings into environment variables

--- env_setup.sh
+++ env_setup.sh
@@ -1,1 +1,9 @@
-export DJANGO_POSTGRES_PASSWORD='<password here>'
+# Django
+export DJANGO_SECRET_KEY=''
+
+# Postgres
+export DJANGO_POSTGRES_PASSWORD=''
+export DJANGO_POSTGRES_HOST=''
+
+# Elasticsearch
+export DJANGO_ELASTICSEARCH_HOST=''
