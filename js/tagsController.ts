4385ce6ec2199992609b07b42c6b1bffc7becfb2
Harry <eldridgemharry@gmail.com>
2019-11-28 21:00:39 -0500

    Reformat js

--- js/tagsController.ts
+++ js/tagsController.ts
@@ -1,26 +1,26 @@
 function expandTags(expander: HTMLElement) {
-  let hiddenTags = expander.parentElement.getElementsByClassName(
-    'collapsed-tag'
-  );
-  for (let i = 0; i < hiddenTags.length; i++) {
-    removeClass(hiddenTags[i], 'hidden');
-  }
-  expander.getElementsByClassName('fas')[0].className = 'fas fa-level-up-alt';
-  expander.onclick = () => {
-    collapseTags(expander);
-  };
+    let hiddenTags = expander.parentElement.getElementsByClassName(
+        'collapsed-tag'
+    );
+    for (let i = 0; i < hiddenTags.length; i++) {
+        removeClass(hiddenTags[i], 'hidden');
+    }
+    expander.getElementsByClassName('fas')[0].className = 'fas fa-level-up-alt';
+    expander.onclick = () => {
+        collapseTags(expander);
+    };
 }
 
 function collapseTags(collapser: HTMLElement) {
-  let hiddenTags = collapser.parentElement.getElementsByClassName(
-    'collapsed-tag'
-  );
-  for (let i = 0; i < hiddenTags.length; i++) {
-    addClass(hiddenTags[i], 'hidden');
-  }
-  collapser.getElementsByClassName('fas')[0].className =
-    'fas fa-level-down-alt';
-  collapser.onclick = () => {
-    expandTags(collapser);
-  };
+    let hiddenTags = collapser.parentElement.getElementsByClassName(
+        'collapsed-tag'
+    );
+    for (let i = 0; i < hiddenTags.length; i++) {
+        addClass(hiddenTags[i], 'hidden');
+    }
+    collapser.getElementsByClassName('fas')[0].className =
+        'fas fa-level-down-alt';
+    collapser.onclick = () => {
+        expandTags(collapser);
+    };
 }
