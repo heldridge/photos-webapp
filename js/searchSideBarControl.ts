720c457db68414c61fa81b2d43f91d5c03fad6f3
Harry <eldridgemharry@gmail.com>
2020-01-15 22:48:02 -0500

    FORMAT

--- js/searchSideBarControl.ts
+++ js/searchSideBarControl.ts
@@ -1,88 +1,88 @@
-let addTagsButton = <HTMLAnchorElement>document.getElementById('addTagsButton');
-let addTagsInput = <HTMLInputElement>document.getElementById('addTagsInput');
+let addTagsButton = <HTMLAnchorElement>document.getElementById("addTagsButton");
+let addTagsInput = <HTMLInputElement>document.getElementById("addTagsInput");
 let addTagsInputContainer = <HTMLDivElement>(
-    document.getElementById('addTagsInputContainer')
+  document.getElementById("addTagsInputContainer")
 );
 let addTagsErrorMessage = <HTMLParagraphElement>(
-    document.getElementById('addTagsErrorMessage')
+  document.getElementById("addTagsErrorMessage")
 );
-let originalHREF = '';
+let originalHREF = "";
 if (addTagsButton !== null) {
-    originalHREF = addTagsButton.href;
+  originalHREF = addTagsButton.href;
 }
 
 function addTagsInputOnKeyUp(
-    e: onKeyUpEvent,
-    currentQuery: string,
-    maxTagLength: number,
-    minTagLength: number,
-    validTagRegex: string
+  e: onKeyUpEvent,
+  currentQuery: string,
+  maxTagLength: number,
+  minTagLength: number,
+  validTagRegex: string
 ) {
-    // Listens for an "Enter" keypress in the add tags input,
-    // on one directs the page to the page + tag
-    if (e.key === 'Enter') {
-        let value = addTagsInput.value;
-        if (value !== '') {
-            // Don't change pages if input is empty
-            let valid = validateTag(
-                value,
-                maxTagLength,
-                minTagLength,
-                new RegExp(validTagRegex)
-            );
-
-            if (valid.isValid) {
-                // Only change pages if tag is valid
-                let newHREF = originalHREF;
-                if (currentQuery !== '') {
-                    newHREF += '+';
-                }
-                newHREF += value;
-                window.location.href = newHREF;
-            }
-        }
-    }
-}
-
-function addTagsInputUpdated(
-    currentQuery: string,
-    maxTagLength: number,
-    minTagLength: number,
-    validTagRegex: string
-) {
-    // Checks tag input to display message if tag is invalid
+  // Listens for an "Enter" keypress in the add tags input,
+  // on one directs the page to the page + tag
+  if (e.key === "Enter") {
     let value = addTagsInput.value;
-
-    // Disable link if tag is empty
-    if (value === '') {
-        addClass(addTagsButton, 'disabled-link');
-    }
-
-    let valid = validateTag(
+    if (value !== "") {
+      // Don't change pages if input is empty
+      let valid = validateTag(
         value,
         maxTagLength,
         minTagLength,
         new RegExp(validTagRegex)
-    );
-    if (valid.isValid) {
-        addClass(addTagsInputContainer, 'border-gray-300');
-        removeClass(addTagsInputContainer, 'border-red-500');
-        addClass(addTagsErrorMessage, 'hidden');
+      );
+
+      if (valid.isValid) {
+        // Only change pages if tag is valid
         let newHREF = originalHREF;
-        if (currentQuery !== '' && value !== '') {
-            newHREF += '+';
+        if (currentQuery !== "") {
+          newHREF += "+";
         }
         newHREF += value;
-        addTagsButton.href = newHREF;
-
-        if (value !== '') {
-            removeClass(addTagsButton, 'disabled-link');
-        }
-    } else if (value !== '') {
-        addTagsErrorMessage.innerHTML = valid.message;
-        removeClass(addTagsInputContainer, 'border-gray-300');
-        addClass(addTagsInputContainer, 'border-red-500');
-        removeClass(addTagsErrorMessage, 'hidden');
-        addClass(addTagsButton, 'disabled-link');
+        window.location.href = newHREF;
+      }
     }
+  }
+}
+
+function addTagsInputUpdated(
+  currentQuery: string,
+  maxTagLength: number,
+  minTagLength: number,
+  validTagRegex: string
+) {
+  // Checks tag input to display message if tag is invalid
+  let value = addTagsInput.value;
+
+  // Disable link if tag is empty
+  if (value === "") {
+    addClass(addTagsButton, "disabled-link");
+  }
+
+  let valid = validateTag(
+    value,
+    maxTagLength,
+    minTagLength,
+    new RegExp(validTagRegex)
+  );
+  if (valid.isValid) {
+    addClass(addTagsInputContainer, "border-gray-300");
+    removeClass(addTagsInputContainer, "border-red-500");
+    addClass(addTagsErrorMessage, "hidden");
+    let newHREF = originalHREF;
+    if (currentQuery !== "" && value !== "") {
+      newHREF += "+";
+    }
+    newHREF += value;
+    addTagsButton.href = newHREF;
+
+    if (value !== "") {
+      removeClass(addTagsButton, "disabled-link");
+    }
+  } else if (value !== "") {
+    addTagsErrorMessage.innerHTML = valid.message;
+    removeClass(addTagsInputContainer, "border-gray-300");
+    addClass(addTagsInputContainer, "border-red-500");
+    removeClass(addTagsErrorMessage, "hidden");
+    addClass(addTagsButton, "disabled-link");
+  }
 }
