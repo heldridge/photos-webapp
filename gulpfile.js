13a345d18b9cf8fe4cc4e74eaaffc7c3d7e0128b
Harry <MerlinCibula@protonmail.com>
2020-01-24 14:20:10 -0500

    Add tailwind config

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,77 @@
-const gulp = require("gulp");
-const { watch, series } = require("gulp");
-const cleanCSS = require("gulp-clean-css");
+const gulp = require('gulp');
+const { watch, series } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
 function processTailwind(cb) {
-  const postcss = require("gulp-postcss");
+    const postcss = require('gulp-postcss');
+    var tailwindcss = require('tailwindcss');
 
-  return gulp
-    .src("css/styles-tailwind.css")
-    .pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("django/photos/static/css"));
+    return (
+        gulp
+            .src('css/styles-tailwind.css')
+            .pipe(
+                postcss([
+                    tailwindcss('./tailwind.config.js'),
+                    require('autoprefixer')
+                ])
+            )
+            // .pipe(cleanCSS())
+            .pipe(gulp.dest('django/photos/static/css'))
+    );
 }
 
 function processJavascript(cb) {
-  const terser = require("gulp-terser");
-  const concat = require("gulp-concat");
-  const ts = require("gulp-typescript");
-  const merge = require("merge-stream");
-  const jsFiles = [
-    {
-      outputFileName: "index.js",
-      files: [
-        "js/imagesGridFooterControl.ts",
-        "js/navbarControl.ts",
-        "js/tagsControl.ts",
-        "js/classUtils.ts"
-      ]
-    },
-    {
-      outputFileName: "search.js",
-      files: [
-        "js/imagesGridFooterControl.ts",
-        "js/navbarControl.ts",
-        "js/tagsControl.ts",
-        "js/searchSideBarControl.ts",
-        "js/classUtils.ts"
-      ]
-    },
-    {
-      outputFileName: "gallery.js",
-      files: [
-        "js/navbarControl.ts",
-        "js/galleryControl.ts",
-        "js/getUrlParameter.ts",
-        "js/classUtils.ts"
-      ]
-    }
-  ];
+    const terser = require('gulp-terser');
+    const concat = require('gulp-concat');
+    const ts = require('gulp-typescript');
+    const merge = require('merge-stream');
+    const jsFiles = [
+        {
+            outputFileName: 'index.js',
+            files: [
+                'js/imagesGridFooterControl.ts',
+                'js/navbarControl.ts',
+                'js/tagsControl.ts',
+                'js/classUtils.ts'
+            ]
+        },
+        {
+            outputFileName: 'search.js',
+            files: [
+                'js/imagesGridFooterControl.ts',
+                'js/navbarControl.ts',
+                'js/tagsControl.ts',
+                'js/searchSideBarControl.ts',
+                'js/classUtils.ts'
+            ]
+        },
+        {
+            outputFileName: 'gallery.js',
+            files: [
+                'js/navbarControl.ts',
+                'js/galleryControl.ts',
+                'js/getUrlParameter.ts',
+                'js/classUtils.ts'
+            ]
+        }
+    ];
 
-  let tasks = jsFiles.map(data =>
-    gulp
-      .src(data.files)
-      .pipe(ts())
-      .pipe(concat(data.outputFileName))
-      .pipe(terser())
-      .pipe(gulp.dest("django/photos/static/js"))
-  );
+    let tasks = jsFiles.map(data =>
+        gulp
+            .src(data.files)
+            .pipe(ts())
+            .pipe(concat(data.outputFileName))
+            .pipe(terser())
+            .pipe(gulp.dest('django/photos/static/js'))
+    );
 
-  return merge(tasks);
+    return merge(tasks);
 }
 
 // exports.default = series(processTailwind, processJavascript);
 
 exports.default = function() {
-  watch("css/*.css", processTailwind);
-  watch("js/*.ts", processJavascript);
+    watch('css/*.css', processTailwind);
+    watch('tailwind.config.js', processTailwind);
+    watch('js/*.ts', processJavascript);
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    '200': '#ffa48c',
+                }
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
