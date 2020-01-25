979c14f958987b6bb2bb4aaa0ab7c6826b076318
Harry <MerlinCibula@protonmail.com>
2020-01-24 19:16:20 -0500

    Move coloring to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
         extend: {
             colors: {
                 primary: {
-                    '200': '#ffa48c',
+                    '200': '#ffa48c'
+                },
+                secondary: {
+                    '200': '#00f2f2'
+                },
+                basic: {
+                    '200': '#f2f2f2',
+                    '300': '#e9e9e9',
+                    '400': '#c7c7c7',
+                    '500': '#a9a9a9',
+                    '600': '#7f7f7f',
+                    '700': '#6b6b6b'
                 }
             }
         }
