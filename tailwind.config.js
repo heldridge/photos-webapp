2ba15792a3e8e1cc4722763be095c6f06edf796c
Harry Eldridge <heldridge@protonmail.com>
2020-01-28 17:32:13 -0500

    Navbar input border responds to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,67 @@
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                primary: {
-                    '50': '#ffe7e6',
-                    '100': '#ffc7b8',
-                    '200': '#ffa28a',
-                    '300': '#ff795b',
-                    '400': '#ff5436',
-                    '500': '#ff200c'
-                },
-                secondary: {
-                    '50': '#d7fcfb',
-                    '100': '#97f7f5',
-                    '200': '#00f2f2',
-                    '300': '#00e9ee',
-                    '400': '#00e1e9',
-                    '500': '#00d9e9',
-                    '600': '#00c8d5',
-                    '700': '#00b2b9',
-                    '800': '#009da0',
-                    '900': '#007870'
-                },
-                basic: {
-                    '200': '#e9eeee',
-                    '300': '#dbdfdf',
-                    '400': '#b8bcbc',
-                    '500': '#999d9d',
-                    '600': '#707474',
-                    '700': '#5c6060'
-                },
-                dt: {
-                    '0': '#121212',
-                    '1': '#1e1e1e',
-                    '2': '#232323',
-                    '3': '#252525',
-                    '4': '#282828',
-                    '5': '#2c2c2c',
-                    '6': '#2F2F2F',
-                    '7': '#343434',
-                    '8': '#363636',
-                    '9': '#383838'
-                }
-            },
-            textColor: {
-                error: 'var(--color-error-text)',
-                primary: 'var(--color-text)'
-            },
-            opacity: {
-                'high-emphasis': 'var(--opacity-text-high-emphasis)',
-                'medium-emphasis': 'var(--opacity-text-medium-emphasis)',
-                disabled: 'var(--opacity-text-disabled)'
-            },
-            backgroundColor: {
-                main: 'var(--color-bg-main)',
-                'surface-1': 'var(--color-bg-surface-1)'
-            }
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "50": "#ffe7e6",
+          "100": "#ffc7b8",
+          "200": "#ffa28a",
+          "300": "#ff795b",
+          "400": "#ff5436",
+          "500": "#ff200c"
+        },
+        secondary: {
+          "50": "#d7fcfb",
+          "100": "#97f7f5",
+          "200": "#00f2f2",
+          "300": "#00e9ee",
+          "400": "#00e1e9",
+          "500": "#00d9e9",
+          "600": "#00c8d5",
+          "700": "#00b2b9",
+          "800": "#009da0",
+          "900": "#007870"
+        },
+        basic: {
+          "200": "#e9eeee",
+          "300": "#dbdfdf",
+          "400": "#b8bcbc",
+          "500": "#999d9d",
+          "600": "#707474",
+          "700": "#5c6060"
+        },
+        dt: {
+          "0": "#121212",
+          "1": "#1e1e1e",
+          "2": "#232323",
+          "3": "#252525",
+          "4": "#282828",
+          "5": "#2c2c2c",
+          "6": "#2F2F2F",
+          "7": "#343434",
+          "8": "#363636",
+          "9": "#383838"
         }
-    },
-    variants: {},
-    plugins: []
+      },
+      textColor: {
+        error: "var(--color-error-text)",
+        primary: "var(--color-text)"
+      },
+      opacity: {
+        "high-emphasis": "var(--opacity-text-high-emphasis)",
+        "medium-emphasis": "var(--opacity-text-medium-emphasis)",
+        disabled: "var(--opacity-text-disabled)"
+      },
+      backgroundColor: {
+        main: "var(--color-bg-main)",
+        "surface-1": "var(--color-bg-surface-1)"
+      },
+      borderColor: {
+        default: "var(--color-border-default)",
+        "input-selected": "var(--color-border-input-selected)"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
 };
