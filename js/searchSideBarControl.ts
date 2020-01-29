274c5226603d33181f91c3b3edd0fbaaba8c04b6
Harry <MerlinCibula@protonmail.com>
2020-01-29 17:08:13 -0500

    Pass valid tag params from js vs function calls
    I want to be able to call the updated() function right away, so the
    valid tag params have to be in the js itself

--- js/searchSideBarControl.ts
+++ js/searchSideBarControl.ts
@@ -1,137 +1,135 @@
-let addTagsIconEnabledClasses = ["text-black"];
-let addTagsIconDisabledClass = "opacity-disabled";
+// @ts-ignore
+let currentQuery = currentQueryOutside;
+// @ts-ignore
+let maxTagLength = maxTagLengthOutside;
+// @ts-ignore
+let minTagLength = minTagLengthOutside;
+// @ts-ignore
+let validTagRegex = validTagRegexOutside;
 
-let addTagsButtonEnabledClasses = ["bg-primary", "hover:bg-primary-hover"];
+let addTagsIconEnabledClasses = ['text-black'];
+let addTagsIconDisabledClass = 'opacity-disabled';
+
+let addTagsButtonEnabledClasses = ['bg-primary', 'hover:bg-primary-hover'];
 let addTagsButtonDisabledClasses = [
-  "bg-button-disabled",
-  "pointer-events-none"
+    'bg-button-disabled',
+    'pointer-events-none'
 ];
 
-let addTagsInputContainerDefaultClass = "border-default";
-let addTagsInputContainerErrorClass = "border-error";
+let addTagsInputContainerDefaultClass = 'border-default';
+let addTagsInputContainerErrorClass = 'border-error';
 
-let addTagsButton = <HTMLAnchorElement>document.getElementById("addTagsButton");
-let addTagsIcon = document.getElementById("addTagsIcon");
-let addTagsInput = <HTMLInputElement>document.getElementById("addTagsInput");
+let addTagsButton = <HTMLAnchorElement>document.getElementById('addTagsButton');
+let addTagsIcon = document.getElementById('addTagsIcon');
+let addTagsInput = <HTMLInputElement>document.getElementById('addTagsInput');
 let addTagsInputContainer = <HTMLDivElement>(
-  document.getElementById("addTagsInputContainer")
+    document.getElementById('addTagsInputContainer')
 );
 let addTagsErrorMessage = <HTMLParagraphElement>(
-  document.getElementById("addTagsErrorMessage")
+    document.getElementById('addTagsErrorMessage')
 );
-let originalHREF = "";
+let originalHREF = '';
 if (addTagsButton !== null) {
-  originalHREF = addTagsButton.href;
+    originalHREF = addTagsButton.href;
 }
 
-function addTagsInputOnKeyUp(
-  e: onKeyUpEvent,
-  currentQuery: string,
-  maxTagLength: number,
-  minTagLength: number,
-  validTagRegex: string
-) {
-  // Listens for an "Enter" keypress in the add tags input,
-  // on one directs the page to the page + tag
-  if (e.key === "Enter") {
+function addTagsInputOnKeyUp(e: onKeyUpEvent) {
+    // Listens for an "Enter" keypress in the add tags input,
+    // on one directs the page to the page + tag
+    if (e.key === 'Enter') {
+        let value = addTagsInput.value;
+        if (value !== '') {
+            // Don't change pages if input is empty
+            let valid = validateTag(
+                value,
+                maxTagLength,
+                minTagLength,
+                new RegExp(validTagRegex)
+            );
+
+            if (valid.isValid) {
+                // Only change pages if tag is valid
+                let newHREF = originalHREF;
+                if (currentQuery !== '') {
+                    newHREF += '+';
+                }
+                newHREF += value;
+                window.location.href = newHREF;
+            }
+        }
+    }
+}
+
+function addTagsInputUpdated() {
+    // Checks tag input to display message if tag is invalid
     let value = addTagsInput.value;
-    if (value !== "") {
-      // Don't change pages if input is empty
-      let valid = validateTag(
+
+    // Disable link if tag is empty
+    if (value === '') {
+        addTagsButtonEnabledClasses.forEach(className => {
+            removeClass(addTagsButton, className);
+        });
+        addTagsButtonDisabledClasses.forEach(className => {
+            addClass(addTagsButton, className);
+        });
+
+        addTagsIconEnabledClasses.forEach(className => {
+            removeClass(addTagsIcon, className);
+        });
+        addClass(addTagsIcon, addTagsIconDisabledClass);
+    }
+
+    let valid = validateTag(
         value,
         maxTagLength,
         minTagLength,
         new RegExp(validTagRegex)
-      );
+    );
+    if (valid.isValid) {
+        addClass(addTagsInputContainer, addTagsInputContainerDefaultClass);
+        removeClass(addTagsInputContainer, addTagsInputContainerErrorClass);
+        addClass(addTagsErrorMessage, 'hidden');
 
-      if (valid.isValid) {
-        // Only change pages if tag is valid
         let newHREF = originalHREF;
-        if (currentQuery !== "") {
-          newHREF += "+";
+        if (currentQuery !== '' && value !== '') {
+            newHREF += '+';
         }
         newHREF += value;
-        window.location.href = newHREF;
-      }
+        addTagsButton.href = newHREF;
+
+        if (value !== '') {
+            addTagsButtonDisabledClasses.forEach(className => {
+                console.log('removing ' + className);
+                removeClass(addTagsButton, className);
+            });
+
+            addTagsButtonEnabledClasses.forEach(className => {
+                addClass(addTagsButton, className);
+            });
+
+            removeClass(addTagsIcon, addTagsIconDisabledClass);
+
+            addTagsIconEnabledClasses.forEach(className => {
+                addClass(addTagsIcon, className);
+            });
+        }
+    } else if (value !== '') {
+        addTagsErrorMessage.innerHTML = valid.message;
+        removeClass(addTagsInputContainer, addTagsInputContainerDefaultClass);
+        addClass(addTagsInputContainer, addTagsInputContainerErrorClass);
+
+        removeClass(addTagsErrorMessage, 'hidden');
+
+        addTagsButtonEnabledClasses.forEach(className => {
+            removeClass(addTagsButton, className);
+        });
+        addTagsButtonDisabledClasses.forEach(className => {
+            addClass(addTagsButton, className);
+        });
+
+        addTagsIconEnabledClasses.forEach(className => {
+            removeClass(addTagsIcon, className);
+        });
+        addClass(addTagsIcon, addTagsIconDisabledClass);
     }
-  }
-}
-
-function addTagsInputUpdated(
-  currentQuery: string,
-  maxTagLength: number,
-  minTagLength: number,
-  validTagRegex: string
-) {
-  // Checks tag input to display message if tag is invalid
-  let value = addTagsInput.value;
-
-  // Disable link if tag is empty
-  if (value === "") {
-    addTagsButtonEnabledClasses.forEach(className => {
-      removeClass(addTagsButton, className);
-    });
-    addTagsButtonDisabledClasses.forEach(className => {
-      addClass(addTagsButton, className);
-    });
-
-    addTagsIconEnabledClasses.forEach(className => {
-      removeClass(addTagsIcon, className);
-    });
-    addClass(addTagsIcon, addTagsIconDisabledClass);
-  }
-
-  let valid = validateTag(
-    value,
-    maxTagLength,
-    minTagLength,
-    new RegExp(validTagRegex)
-  );
-  if (valid.isValid) {
-    addClass(addTagsInputContainer, addTagsInputContainerDefaultClass);
-    removeClass(addTagsInputContainer, addTagsInputContainerErrorClass);
-    addClass(addTagsErrorMessage, "hidden");
-
-    let newHREF = originalHREF;
-    if (currentQuery !== "" && value !== "") {
-      newHREF += "+";
-    }
-    newHREF += value;
-    addTagsButton.href = newHREF;
-
-    if (value !== "") {
-      addTagsButtonDisabledClasses.forEach(className => {
-        console.log("removing " + className);
-        removeClass(addTagsButton, className);
-      });
-
-      addTagsButtonEnabledClasses.forEach(className => {
-        addClass(addTagsButton, className);
-      });
-
-      removeClass(addTagsIcon, addTagsIconDisabledClass);
-
-      addTagsIconEnabledClasses.forEach(className => {
-        addClass(addTagsIcon, className);
-      });
-    }
-  } else if (value !== "") {
-    addTagsErrorMessage.innerHTML = valid.message;
-    removeClass(addTagsInputContainer, addTagsInputContainerDefaultClass);
-    addClass(addTagsInputContainer, addTagsInputContainerErrorClass);
-
-    removeClass(addTagsErrorMessage, "hidden");
-
-    addTagsButtonEnabledClasses.forEach(className => {
-      removeClass(addTagsButton, className);
-    });
-    addTagsButtonDisabledClasses.forEach(className => {
-      addClass(addTagsButton, className);
-    });
-
-    addTagsIconEnabledClasses.forEach(className => {
-      removeClass(addTagsIcon, className);
-    });
-    addClass(addTagsIcon, addTagsIconDisabledClass);
-  }
 }
