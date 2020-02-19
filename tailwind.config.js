86d7e5a95abc63ee7152d2723b9d9cbb76d79f05
Harry <MerlinCibula@protonmail.com>
2020-02-19 18:30:11 -0500

    Add errors to login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -77,6 +77,9 @@ module.exports = {
             },
             minHeight: {
                 'double-screen': '200vh'
+            },
+            width: {
+                '77': '19.25rem'
             }
         }
     },
