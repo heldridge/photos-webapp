5f56ce25319f3af227a09ba3e72316088384d14d
Harry Eldridge <heldridge@protonmail.com>
2020-02-24 21:22:02 -0500

    Add display_name to registration form

--- django/users/forms.py
+++ django/users/forms.py
@@ -12,6 +12,13 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.fields["display_name"].widget.attrs.update(
+            {
+                "class": "focus:outline-none focus:border-input-selected appearance-none py-3 bg-surface-1 opacity-high-emphasis rounded border-2 border-default text-indent shadow-none border-color-transition disable-transition-on-theme-change",
+                # "onfocusout": "addInvalidBorder(this)",
+            }
+        )
+
         self.fields["email"].widget.attrs.update(
             {
                 "class": "focus:outline-none focus:border-input-selected appearance-none py-3 bg-surface-1 opacity-high-emphasis rounded border-2 border-default text-indent shadow-none border-color-transition disable-transition-on-theme-change",
