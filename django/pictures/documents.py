6482e29e86cd77e67ae00a37142a38c1c280af9b
Harry <MerlinCibula@protonmail.com>
2020-03-12 18:26:13 -0400

    Prep to move query logic to models / documents

--- django/pictures/documents.py
+++ django/pictures/documents.py
@@ -1,4 +1,6 @@
 # documents.py
+from django.core import exceptions
+
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from .models import Picture
@@ -33,3 +35,76 @@ class PictureDocument(Document):
             "uploaded_at",
             "updated_at",
         ]
+
+
+def get_pictures(amount, before=None, after=None, tags=None):
+    """Queries the database or elasticsearch for pictures
+
+    Args:
+        amount (``int``): The number of pictures to return
+        before (``str``): The id of the photo to fetch photos before
+        after (``str``): The id of the photo to fetch photos after
+        tags (``list`` of ``str``): The list of tags to search for
+
+    Returns:
+        {
+            "result": A list of pictures,
+            "more_left": (``bool``) Whether there are more 
+                         pictures remaining with the given query specification.
+        }
+
+    Raises:
+        ``ValueError``: If both before and after are specified
+    """
+    if before is not None and after is not None:
+        raise ValueError("Only one of before and after can be specified")
+
+    # Fetch the correct before or after picture
+    if before is not None and before != "":
+        try:
+            before_picture = Picture.objects.get(public_id=before)
+        except (exceptions.ValidationError, Picture.DoesNotExist):
+            before = None
+    if after is not None and after != "":
+        try:
+            after_picture = Picture.objects.get(public_id=after)
+        except (exceptions.ValidationError, Picture.DoesNotExist):
+            after = None
+
+    # If tags is None we can get everything from the database
+    if tags is None:
+        query_set = Picture.objects
+        if before is not None:
+            # For before we want to go "backwards," to get the pictures
+            # closest in id to before_picture, so order by id (increasing)
+            query_set = list(query_set.filter(id__gt=before_picture.id).order_by("id"))
+        elif after is not None:
+            query_set = query_set.filter(id__lt=after_picture.id).order_by("-id")
+        else:
+            query_set = query_set.order_by("-id")
+
+    # If there are tags we have to go to Elasticsearch
+    else:
+        query_set = PictureDocument.search()
+
+        for tag in tags:
+            query_set = query_set.query("term", tags=tag)
+
+        if before is not None:
+            query_set = query_set.query("range", id={"gt": before_picture.id}).sort(
+                "id"
+            )
+        elif after is not None:
+            query_set = query_set.query("range", id={"lt": after_picture.id}).sort(
+                "-id"
+            )
+        else:
+            query_set = query_set.sort("-id")
+
+    query_set = query_set[: amount + 1]
+
+    # We went "backwards" if we had a before, so reverse the list now
+    if before is not None:
+        query_set.reverse()
+
+    return {"pictures": query_set[:amount], "more_left": len(query_set) < amount + 1}
--- django/pictures/models.py
+++ django/pictures/models.py
@@ -39,3 +39,51 @@ class Favorite(models.Model):
     user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
     picture = models.ForeignKey("Picture", on_delete=models.CASCADE)
 
+
+###################
+# UTILITY METHODS #
+###################
+def normalize_photo(picture):
+    """
+    Normalizes the 'photo' field for Picture Models and Documents
+
+    The Picture Model and the Picture Document handle the 'photo' field
+    differently, which can lead to trickyness down the road.
+    This method normalizes that field so that it is consistent.
+
+    Args:
+        picture (``str`` or ``ImageField``): the field value to normalize
+
+    Returns:
+        ``str``: the str value of the photo's filename
+    """
+    return str(picture.photo)
+
+
+def get_split_tags(tags):
+    """
+    Splits an image's tags into "above" and "below" tags,
+    so they don't over-clutter the UI
+
+    Above and below are decided by estimating how wide
+    the resulting tag bar would be, and then putting a cap on
+    that width
+    """
+    data = {"above_tags": [], "below_tags": []}
+
+    max_width = 85  # Max width of the theoretical tag bar
+    expander_length = 5  # Width of the "click to expand" symbol
+    static_width_addition = 4  # How much to add in addition to each letter
+    current_width = 0
+    above = True
+    for tag in tags:
+        current_width += static_width_addition + len(tag)
+        if current_width + expander_length > max_width:
+            above = False
+
+        if above:
+            data["above_tags"].append(tag)
+        else:
+            data["below_tags"].append(tag)
+    return data
+
