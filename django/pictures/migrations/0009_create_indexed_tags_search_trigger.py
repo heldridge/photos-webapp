0317b7e4e3287fcc1ed77bdf2d51cb9e3f1b9f04
Harry Eldridge <heldridge@protonmail.com>
2020-03-15 23:21:34 -0400

    Add in index to do full text search

---
+++ django/pictures/migrations/0009_create_indexed_tags_search_trigger.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("pictures", "0008_auto_20200316_0156"),
+    ]
+
+    migration = """
+        CREATE TRIGGER tagsearchupdate BEFORE INSERT OR UPDATE
+        ON pictures_picture FOR EACH ROW EXECUTE PROCEDURE
+        tsvector_update_trigger(indexed_tags_search, 'pg_catalog.english', tags);
+
+        -- Force triggers to run and populate the text_search column.
+        UPDATE pictures_picture set ID = ID;
+    """
+
+    reverse_migration = """
+        DROP TRIGGER tagsearchupdate ON pictures_picture;
+    """
+
+    operations = [migrations.RunSQL(migration, reverse_migration)]
