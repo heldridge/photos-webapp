6a84ff98a77cfbf3859fb28d1d098212f5ff7712
Harry <eldridgemharry@gmail.com>
2020-03-23 17:09:00 -0400

    Add uploaded by field and remove datetimes in loadjson

--- django/pictures/management/commands/loadjson.py
+++ django/pictures/management/commands/loadjson.py
@@ -4,6 +4,7 @@ import json
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from pictures.models import Picture
+from users.models import CustomUser
 
 
 class Command(BaseCommand):
@@ -12,21 +13,29 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("data_file", help="The path to the json file to load")
         parser.add_argument("--limit", help="Limit the number of images loaded")
+        parser.add_argument(
+            "--uploaded_by_email",
+            help="The email of the user to mark each image as uploaded by",
+            default="harry@example.com",
+        )
 
     def handle(self, *args, **kwargs):
+        uploaded_by_user = CustomUser.objects.get(email=kwargs["uploaded_by_email"])
+
         with open(kwargs["data_file"], "r") as infile:
             data = json.load(infile)
 
         if kwargs["limit"]:
             data = data[: kwargs["limit"]]
 
-        for image in data:
+        for index, image in enumerate(data):
+            if index % 50 == 0:
+                print(index)
             picture = Picture.objects.create(
                 title=image["title"],
                 description="description",
                 photo=f'pictures/2020/01/01/{image["filename"]}',
-                uploaded_at=datetime.datetime.now(),
-                updated_at=datetime.datetime.now(),
                 tags=" ".join(image["tags"]),
+                uploaded_by=uploaded_by_user,
             )
             picture.save()
