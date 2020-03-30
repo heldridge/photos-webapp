025c7434f8f5484a2a6ba180cebfead36ead14f8
Harry <MerlinCibula@protonmail.com>
2020-03-29 21:41:24 -0400

    Only add spaces if necessary in popular tags on upload

--- js/upload.ts
+++ js/upload.ts
@@ -1,5 +1,12 @@
 function addPopularTag(source: HTMLButtonElement) {
     let tagsInput = <HTMLInputElement>document.getElementById('id_tags');
     let title = source.getElementsByTagName('span')[0];
-    tagsInput.value = tagsInput.value + ' ' + title.innerHTML;
+
+    if (tagsInput.value.length === 0) {
+        tagsInput.value = title.innerHTML;
+    } else if (tagsInput.value.slice(-1) === ' ') {
+        tagsInput.value = tagsInput.value + title.innerHTML;
+    } else {
+        tagsInput.value = tagsInput.value + ' ' + title.innerHTML;
+    }
 }
