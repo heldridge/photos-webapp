1c92b4908c36f128ab4694459a9b577ec02a9093
Harry <MerlinCibula@protonmail.com>
2020-03-29 20:46:30 -0400

    Add updatetagstable command

---
+++ django/pictures/management/commands/updatetagstable.py
@@ -0,0 +1,23 @@
+import collections
+
+from django.core.management.base import BaseCommand
+from pictures.models import Picture, Tag
+
+
+class Command(BaseCommand):
+    help = "Updates the tags table based on all the tags in the pictures table"
+
+    def handle(self, *args, **kwargs):
+
+        counter = collections.Counter()
+        for tags in [picture.tags for picture in Picture.objects.all()]:
+            counter.update(tags.split())
+
+        progress = 0
+        for title, count in dict(counter).items():
+            tag, _ = Tag.objects.get_or_create(title=title)
+            tag.count = count
+            tag.save()
+            progress += 1
+            if progress % 50 == 0:
+                print(progress)
