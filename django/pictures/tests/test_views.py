a257f510030d2757e1b20d28c89df9b5922d2611
Harry <MerlinCibula@protonmail.com>
2020-03-30 16:16:13 -0400

    Add favorites adding tests

--- django/pictures/tests/test_views.py
+++ django/pictures/tests/test_views.py
@@ -3,7 +3,8 @@ import uuid
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from pictures.models import Picture
+from pictures.models import Picture, Favorite
+from users.models import CustomUser
 
 
 class TestPagesLoad(TestCase):
@@ -37,3 +38,36 @@ class TestPagesLoad(TestCase):
     def test_picture_bad_uuid(self):
         response = self.client.get(f"/pictures/bad")
         self.assertEqual(response.status_code, 200)
+
+
+class TestFavorites(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        num_pictures = 1
+        for _ in range(num_pictures):
+            Picture.objects.create(
+                title="Test Title",
+                description="Desc",
+                tags="a b c d",
+                photo=SimpleUploadedFile(
+                    name="picture1.jpg",
+                    content=open("test_data/picture1.jpg", "rb").read(),
+                ),
+            )
+
+        cls.picture = Picture.objects.all()[0]
+
+        cls.user = CustomUser.objects.create(email="test@example.com", password="test")
+
+    def test_add_favorite(self):
+        self.client.force_login(self.user)
+        response = self.client.post(f"/pictures/{self.picture.public_id}/favorites/")
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(
+            Favorite.objects.filter(picture=self.picture, user=self.user).exists()
+        )
+
+    def test_add_favorite_not_logged_in(self):
+        response = self.client.post(f"/pictures/{self.picture.public_id}/favorites/")
+        self.assertEqual(response.status_code, 401)
+        self.assertTrue(len(Favorite.objects.all()) == 0)
