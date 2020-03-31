7d3b5a2714fb024e70bc109d59674a0eab9d41e1
Harry <eldridgemharry@gmail.com>
2020-03-31 16:52:35 -0400

    Add a tf backend file to the web tf

---
+++ infrastructure/vpc_resources/web/terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket  = "terraform-remote-state-prod-20200131000633966100000001"
+    region  = "us-east-1"
+    key     = "web-main/terraform.tfstate"
+  }
+}
