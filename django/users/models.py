402ee7603622458ac8a922cce2ab1e4ed5ad61b0
Harry <eldridgemharry@gmail.com>
2020-04-06 14:47:36 -0400

    Add last_email_dates field to CustomUser

--- django/users/models.py
+++ django/users/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
@@ -13,6 +14,7 @@ class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=30, default="Unknown")
     public_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     email_confirmed = models.BooleanField(default=False)
+    last_email_dates = ArrayField(models.DateField(blank=True), size=5, default=list)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
