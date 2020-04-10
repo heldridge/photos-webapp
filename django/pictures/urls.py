61506d12f5468f4fc3d75f27b2a9b235fb1517ab
Harry <eldridgemharry@gmail.com>
2020-04-10 18:34:08 -0400

    Add delete button functionality

--- django/pictures/urls.py
+++ django/pictures/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("delete-success", views.delete_success, name="delete_success"),
     path("<str:picture_public_id>", views.PictureView.as_view(), name="picture"),
     path("<str:picture_public_id>/favorites/", views.Favorites.as_view()),
     path("upload/", views.Upload.as_view(), name="upload"),
