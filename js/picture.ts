6c17e074728157aae91de8dd8e2058bccdbff39b
Harry <eldridgemharry@gmail.com>
2020-04-10 18:14:17 -0400

    Pop up and remove delete confirm box

--- js/picture.ts
+++ js/picture.ts
@@ -1,9 +1,33 @@
 let publicId = JSON.parse(document.getElementById('publicId').textContent);
 
+let confirmDeleteBox = document.getElementById('confirmDeleteBox');
+let deleteButton = document.getElementById('deleteButton');
+document.addEventListener('click', (event) => {
+    if (
+        //@ts-ignore
+        !confirmDeleteBox.contains(event.target) &&
+        //@ts-ignore
+        !deleteButton.contains(event.target) &&
+        !hasClass(confirmDeleteBox, 'hidden')
+    ) {
+        addClass(confirmDeleteBox, 'hidden');
+    }
+});
+
 function addFavoriteWrapper(source: HTMLButtonElement) {
     addFavorite(source, publicId, () => {});
 }
 
 function removeFavoriteWrapper(source: HTMLButtonElement) {
     removeFavorite(source, publicId, () => {});
+}
+
+function toggleConfirmDeleteBoxVisibility() {
+    if (confirmDeleteBox) {
+        if (hasClass(confirmDeleteBox, 'hidden')) {
+            removeClass(confirmDeleteBox, 'hidden');
+        } else {
+            addClass(confirmDeleteBox, 'hidden');
+        }
+    }
 }
