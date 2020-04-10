e00b30f2a36f059d217ca399120ed4830f1fe403
Harry <eldridgemharry@gmail.com>
2020-04-10 15:14:07 -0400

    Add favorites to admin

--- django/pictures/admin.py
+++ django/pictures/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Picture
+from .models import Picture, Favorite
+
 
 class PictureAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ("title",)
 
 
 admin.site.register(Picture, PictureAdmin)
+admin.site.register(Favorite)
