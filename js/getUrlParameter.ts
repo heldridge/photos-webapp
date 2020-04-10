80a3c5b66e04d865488a464c858cad092ba2a947
Harry <MerlinCibula@protonmail.com>
2020-04-10 11:11:17 -0400

    keeps +'s in getUrlParameter

--- js/getUrlParameter.ts
+++ js/getUrlParameter.ts
@@ -2,7 +2,5 @@ function getUrlParameter(name: string) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
-    return results === null
-        ? ''
-        : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    return results === null ? '' : decodeURIComponent(results[1]);
 }
