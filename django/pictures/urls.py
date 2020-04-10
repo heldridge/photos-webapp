ccfb4467462c9156469a711a63b64f767f74ca36
Harry <eldridgemharry@gmail.com>
2020-04-10 16:30:14 -0400

    Change picture to be a class view

--- django/pictures/urls.py
+++ django/pictures/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:picture_public_id>", views.picture, name="picture"),
+    path("<str:picture_public_id>", views.PictureView.as_view(), name="picture"),
     path("<str:picture_public_id>/favorites/", views.Favorites.as_view()),
     path("upload/", views.Upload.as_view(), name="upload"),
 ]
