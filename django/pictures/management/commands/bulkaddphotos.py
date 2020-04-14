1155bd0f3244d73ef863a506fdbbd6b512da9e4d
Harry <eldridgemharry@gmail.com>
2020-04-14 13:45:11 -0400

    Allow for randomly choosing a user in bulkaddphotos

--- django/pictures/management/commands/bulkaddphotos.py
+++ django/pictures/management/commands/bulkaddphotos.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import os
 import pathlib
+import random
 import uuid
 
 import boto3
@@ -19,13 +20,24 @@ class Command(BaseCommand):
         parser.add_argument("data_file", help="The path to the json file to load")
         parser.add_argument("images_dir", help="path to the directory with the images")
         parser.add_argument("--limit", help="Limit the number of images loaded")
+
         parser.add_argument(
-            "--uploaded_by_email",
-            help="The email of the user to mark each image as uploaded by",
+            "--uploaded_by_emails",
+            nargs="+",
+            help=(
+                "The emails to mark each image as uploaded by. "
+                "If multiple are selected a random email will be "
+                "chosen for each picture"
+            ),
         )
 
     def handle(self, *args, **kwargs):
-        uploaded_by_user = CustomUser.objects.get(email=kwargs["uploaded_by_email"])
+        print(kwargs["uploaded_by_emails"])
+
+        users = [
+            CustomUser.objects.get(email=email)
+            for email in kwargs["uploaded_by_emails"]
+        ]
 
         with open(kwargs["data_file"], "r") as infile:
             data = json.load(infile)
@@ -54,7 +66,7 @@ class Command(BaseCommand):
                 description="description",
                 photo=path_start + public_id + extension,
                 tags=" ".join(image["tags"]),
-                uploaded_by=uploaded_by_user,
+                uploaded_by=random.choice(users),
             )
             picture.save()
 
