4c32523b60d3915c469cb824b1285cdf37b9489c
Harry <eldridgemharry@gmail.com>
2020-04-14 16:09:45 -0400

    Make uploading images optional in bulkaddphotos

--- django/pictures/management/commands/bulkaddphotos.py
+++ django/pictures/management/commands/bulkaddphotos.py
@@ -18,7 +18,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("data_file", help="The path to the json file to load")
-        parser.add_argument("images_dir", help="path to the directory with the images")
+        parser.add_argument(
+            "--images_dir", help="path to the directory with the images"
+        )
         parser.add_argument("--limit", help="Limit the number of images loaded")
 
         parser.add_argument(
@@ -30,10 +32,16 @@ class Command(BaseCommand):
                 "chosen for each picture"
             ),
         )
+        parser.add_argument(
+            "--upload_photos",
+            action="store_true",
+            help=(
+                "Whether to upload the photos to s3. Requires "
+                "--images-dir to be specified"
+            ),
+        )
 
     def handle(self, *args, **kwargs):
-        print(kwargs["uploaded_by_emails"])
-
         users = [
             CustomUser.objects.get(email=email)
             for email in kwargs["uploaded_by_emails"]
@@ -45,26 +53,34 @@ class Command(BaseCommand):
         if kwargs["limit"]:
             data = data[: int(kwargs["limit"])]
 
-        s3 = boto3.resource("s3")
-        bucket = s3.Bucket("media.qlbhmmvpym.club")
+        if kwargs["upload_photos"]:
+            s3 = boto3.resource("s3")
+            bucket = s3.Bucket("media.qlbhmmvpym.club")
 
         for index, image in enumerate(data):
-            public_id = str(uuid.uuid4())
+            if kwargs["upload_photos"]:
+                public_id = str(uuid.uuid4())
+            else:
+                public_id = image["public_id"]
 
             extension = os.path.splitext(image["filename"])[1]
 
             path_start = "pictures/" + datetime.datetime.utcnow().strftime("%Y/%m/%d/")
 
-            bucket.upload_file(
-                str(pathlib.Path(kwargs["images_dir"], image["filename"])),
-                "media/" + path_start + public_id + extension,
-            )
+            if kwargs["upload_photos"]:
+                bucket.upload_file(
+                    str(pathlib.Path(kwargs["images_dir"], image["filename"])),
+                    "media/" + path_start + public_id + extension,
+                )
+                photo = path_start + public_id + extension
+            else:
+                photo = image["photo"]
 
             picture = Picture.objects.create(
                 public_id=public_id,
                 title=image["title"],
                 description="description",
-                photo=path_start + public_id + extension,
+                photo=photo,
                 tags=" ".join(image["tags"]),
                 uploaded_by=random.choice(users),
             )
